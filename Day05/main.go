2e4cb6e050b92ecc323e05945075ff01ab5db6a8
Jacob Verdesi <jverdesi@dekaresearch.com>
2023-12-07 17:06:45 -0500

    D4: Done

---
+++ Day05/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func part1() {
+
+}
+func part2() {
+
+}
+
+func main() {
+	fmt.Println("--- Part 1 ---")
+	part1()
+	fmt.Println("--- Part 2 ---")
+	part2()
+}
